131b0ce6de05725a0e40e688eacfb1aa1f65f8a4
Юрий <yurii.shilkin@gmail.com>
2021-10-04 08:19:20 +0300

    createform

---
+++ project/main.py
@@ -0,0 +1,42 @@
+from PyQt5 import QtWidgets
+from MainForm import Ui_MainWindow
+from PyQt5.QtWidgets import QFileDialog, QLabel, QTableWidgetItem, QHeaderView, QMessageBox
+import sys, os
+
+
+class MyWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.loadVipiski.clicked.connect(self.open_file)
+        self.ui.loadPrilozhenya.clicked.connect(self.open_file)
+        
+    def open_file(self):
+        self.filename = QFileDialog.getOpenFileName(
+        None, 'Открыть', os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop'),
+        'All Files(*.xlsx)')
+
+        sender = self.sender()
+        if str(self.filename) in "('', '')":
+            self.ui.statusbar.showMessage('Файл не выбран')
+        else:
+            if sender.text() == 'Загрузить Выписки':
+                self.filename_one = self.filename
+                self.check_one = True
+                self.check_two = False
+                
+            else:
+                self.filename_two = self.filename
+                self.check_one = False
+                self.check_two = True
+
+                
+
+app = QtWidgets.QApplication([])
+application = MyWindow()
+application.setWindowTitle("Доходы 2")
+application.show()
+
+sys.exit(app.exec())

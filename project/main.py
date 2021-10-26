3a218cb06dcb729317fc91783dd1c631ef4bca4e
Mikhail <8916misha@gmail.com>
2021-10-26 18:45:18 +0300

    update

--- project/main.py
+++ project/main.py
@@ -1,7 +1,8 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from MainForm import Ui_MainWindow
 from PyQt5.QtWidgets import QFileDialog, QLabel, QTableWidgetItem, QHeaderView, QMessageBox
-import sys, os
+import sys
+import os
 from read_excel import Read
 
 
@@ -18,11 +19,13 @@ class MyWindow(QtWidgets.QMainWindow):
         self.filename = ''
         self.filename_one = ''
         self.filename_two = ''
-        
+
     def open_file(self):
+
         self.filename = QFileDialog.getOpenFileName(
-        None, 'Открыть', os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop'),
-        'All Files(*.xlsx)')
+            None, 'Открыть', os.path.join(os.path.join(
+                os.environ['USERPROFILE']), 'Desktop'),
+            'All Files(*.xlsx)')
 
         sender = self.sender()
         if str(self.filename) in "('', '')":
@@ -33,17 +36,23 @@ class MyWindow(QtWidgets.QMainWindow):
                 self.check_one = True
                 self.check_two = False
                 self.new_thread()
-                
+
             else:
                 self.filename_two = self.filename
                 self.check_one = False
                 self.check_two = True
                 self.new_thread()
-
     
+    @QtCore.pyqtSlot(list)
+    def appendText(self, list):
+        for ls in list:
+            self.ui.logger.append(str(ls.licevoy) + "   " + str(ls.summa))
+
     def new_thread(self):
         self.my_thread = Read(my_window=self)
+        self.my_thread.appendText.connect(self.appendText)
         self.my_thread.start()
+
 
 
 app = QtWidgets.QApplication([])
--- project/pojo.py
+++ project/pojo.py
@@ -1,6 +1,8 @@
 class LS():
-    def __init__(self, licevoy, summa):
+    def __init__(self, licevoy, summa = 0, rowNumber = 0):
        self.licevoy = licevoy
        self.summa = summa
-
+       self.rowNumber = rowNumber
     
+    def SetSumma(self,summa):
+        self.summa = summa
